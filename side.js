6f5c2c50810b13223fc2278239e487cafb1cecce
anshusaurav <anshu.saurav@gmail.com>
2020-04-23 23:57:15 +0530

    'Search-done'

--- side.js
+++ side.js
@@ -1,12 +1,19 @@
 //https://api.twitch.tv/helix/videos?user_id=108268890 for videos
 
-let gameNameIdMap = new Map();
+
+
+let channelMainElem = document.querySelector('.streams-list-div');
+let loaderElem = document.querySelector('.search-fail-div');
+let headerElem = document.querySelector('h2');
+let titleElem = document.querySelector('title');
+
 //gameNameIdMap.set()
 
 class SearchMain{
     constructor(str){
         this.user = '';
         this.game = '';
+        this.gameName = '';
         this.flagLang = false;
         this.flagView = false;
         this.lang = '';
@@ -122,7 +129,7 @@ class SearchMain{
         });
         
         feeds = await response.json();
-        console.log('Sunygame', feeds);
+        // console.log('Sunygame', feeds);
     
         if(feeds.data.length > 0)
             return {isDone: true,result: feeds.data};
@@ -232,6 +239,7 @@ class SearchMain{
                 });
                 feedsTwo = await responseTwo.json();
                 // console.log(feedsTwo);
+                loaderElem.style.visibility = 'hidden';
                 if(feedsTwo.data.length > 0)
                     return {isDone: true,result: feedsTwo.data, isLive: true};
                 else
@@ -245,49 +253,46 @@ class SearchMain{
             return  {isDone: false,result: null};
         }
     }
+    async getGameName(){
+        let feeds,response;
+        try{
+        response = await fetch(`https://api.twitch.tv/helix/games?id=${this.game}`,{
+            method:'GET',
+            headers: {
+            'Client-ID': 'iswx80n6way6l4cvuecpmtz3gw75vd'
+            }
+        });
+        
+        feeds = await response.json();
+        if(feeds.data.length){
+            this.gameName = feeds.data[0].name;
+            loaderElem.style.visibility = 'hidden';
+            console.log(val,"jhhjjhj")
+            return ;
+        }
+        return false;
+        }
+        catch(error){
+            console.log(error);
+        }
+    }
+
 }
 
-// async function test(){
-//     let totalStr = sessionStorage.getItem('searchedKey');
-//     let searchEx1 = new SearchMain(totalStr);
-//     // console.log(elem);
-//     if(searchEx1.gFlag == 1){
-//         let res = await searchEx1.getUserByName(searchEx1.user);
-//         console.log(res);
-//     }
-//     else if(searchEx1.gFlag == 2) {
-//         if(searchEx1.flagLang && searchEx1.flagView){
-//             let res = await searchEx1.getGameByGameIdAndLangAndViewer(searchEx1.game);
-//             console.log('1');
-//             console.log(res);
-//         }
-//         else if(searchEx1.flagLang && !searchEx1.flagView){
-//             let res = await searchEx1.getGameByGameIdAndLang(searchEx1.game);
-//             console.log('2');
-//             console.log(res);
-//         }
-//         else if(!searchEx1.flagLang && searchEx1.flagView){
-//             let res = await searchEx1.getGameByGameIdAndView(searchEx1.game);
-//             console.log('3');
-//             console.log(res);
-//         }
-//         else {
-//             let res = await searchEx1.getGameByGameId(searchEx1.game);
-//             console.log('4');
-//             console.log(res);
-//         }
-//     }
-//     else {
-//         console.log(searchEx1.user +' '+ searchEx1.game + ' ' +searchEx1.errorStr);
-//     }
-// }
-let channelMainElem = document.querySelector('.streams-list-div');
-let headerElem = document.querySelector('h2');
+
+
+
+
+// getGameName(29595)
+
+
 headerElem.style.display = 'none';
 async function loadChannels() {
     let totalStr = sessionStorage.getItem('searchedKey');
     let searchEx1 = new SearchMain(totalStr);
+    titleElem.innerHTML = 'Search results for ' + totalStr; 
     // console.log(elem);
+
     if(searchEx1.gFlag == 1){
         let res = await searchEx1.getUserByName();
         // console.log(res);
@@ -339,6 +344,7 @@ async function loadChannels() {
     }
     else if(searchEx1.gFlag == 2) {
         let res;
+        let gameN = await searchEx1.getGameName();
         if(searchEx1.flagLang && searchEx1.flagView){
             res = await searchEx1.getGameByGameIdAndLangAndViewer();
             
@@ -366,7 +372,7 @@ async function loadChannels() {
                 </div>
                 <div class='streamer-details-div'>
                     <a href='https://www.twitch.tv/${elem.user_name}'><h3 class='streamer-name'>${elem.user_name}</h3></a>
-                    <h4 class='stream-game-name'>${elem.game_id}</h4>
+                    <h4 class='stream-game-name'>${searchEx1.gameName}</h4>
                     <h4 class='stream-view-count'>${getCounts(elem.viewer_count)} viewers</h4>
                     <h4 class='stream-title-name'>${elem.title}</h4>
                     <h5 class='Language'>${elem.language}</h5>
@@ -406,6 +412,9 @@ function getCounts(cnt) {
 
 }
 // test();
+function paintLiveChannel(){
+    
+}
 loadChannels();
 
 //https://api.twitch.tv/helix/games?id=29595
@@ -421,294 +430,3 @@ loadChannels();
 }
 */
 
-
-// async function search(){
-//     let isDone = false;
-//     let isGame = false;
-//     let isUser = false;
-//     if(isDone == false){
-        
-//         let totalStr = sessionStorage.getItem('searchedKey');
-//         let arr = totalStr.match(/\S+/g);
-//         console.log(arr);
-//         let str = arr[0];
-//         let obj = matchesGameByGameId(str);
-//         // let h1 = document.createElement('h1');
-//         // h1.innerHTML = str;
-//         // document.body.append(h1);
-//         //console.log(obj.stringify())
-        
-//         if(obj.res == true){
-//             console.log('Valid format for game ' + str);
-//             //console.log(getGameByGameId(obj.result) );
-//             let bool = await getGameByGameId(obj.result);
-//             if(bool)
-//                 console.log('Game-ID-Valid');
-//             else
-//                 console.log('Game-ID not a game');
-//             isDone = true;
-//             isGame = true;
-//         }
-//         else{
-//             console.log('Invalid format for game')
-//         }
-//     }
-
-//     if(isDone == false){
-//         let obj = matchesUserByUserName(str);
-//         //console.log(obj.stringify())
-        
-//         if(obj.res == true){
-//             console.log('Valid format for user ' + obj.result);
-//             //console.log(getGameByGameId(obj.result) );
-//             let qRes = await getStatusByUserName(obj.result);    //return object {user found, but not live or user found and live, user not found}
-//             if(qRes.res){
-//                 if(qRes.isLive)
-//                     console.log(qRes.isLive  + ' Player is live');
-//                 else
-//                     console.log(qRes.isLive  + ' Player is not live');
-//             }
-//             else
-//                 console.log(qRes.isLive + ' Player is live');
-//             isDone = true;
-//             isUser = true;
-//         }
-//         else{
-//             console.log('Invalid format for user')
-//         }
-//     }
-//     if(isDone == false){
-//         let obj = matchesStreamByGameIdAndLang(str);
-//         //console.log(obj.stringify())
-        
-//         if(obj.res == true){
-//             console.log('Valid format for lang ' + obj.result);
-//             //console.log(getGameByGameId(obj.result) );
-//             let qRes = await getStreamByGameIdAndLang(29595,obj.result, 100);   
-//             if(qRes)
-//                 console.log(qRes  + ' Language and game found');
-//             else
-//                 console.log(qRes + ' Error with langauge');
-//             isDone = true;
-//             isGame = true;
-//         }
-//         else{
-//             console.log('Invalid format for user')
-//         }
-//     }
-//     if(isDone == false){
-//         let obj = matchesStreamByGameIdAndViewerCount(str);
-//         //console.log(obj.stringify())
-        
-//         if(obj.res == true){
-//             console.log('Valid format for lang ' + obj.result);
-//             //console.log(getGameByGameId(obj.result) );
-//             let qRes = await getStreamByGameIdAndViewerCount(29595,'en', 100, obj.result);    //return object {user found, but not live or user found and live, user not found}
-
-//             console.log(qRes  + ' # streams with more than ' + obj.result +' viewers');
-            
-//             isDone = true;
-//             isGame = true;
-//         }
-//         else{
-//             console.log('Invalid format for user')
-//         }
-//     }
-// }
-
-//  For game [game-id]
-//  https://api.twitch.tv/helix/streams?game_id=29595
-//Check if game id is perfect in format i.e. inside '[', ']'
-// function matchesGameByGameId(gIdWithBraces) {
-//     if(gIdWithBraces.trim().startsWith ('[') && gIdWithBraces.endsWith(']'))
-//     {
-//         let str = gIdWithBraces.trim();
-//         let res = str.substr(1, str.length-2);
-//         console.log('|'+res+'|');
-//         //ccheck whether res shouldn't have non-numeric characters
-//         let regEx = new RegExp(/^\d+$/);
-//         const found = res.match(regEx);
-//         if(found)
-//             return {res: true, result: res};
-//         return {res: false, result: null};
-//     }
-//     return {res: false,result: null};
-// }
-
-// //Check if a game exists with extracted correct formatted gameid
-// async function getGameByGameId(gId){
-//     console.log(gId);
-//     let feeds,response;
-//     searchFound = false;
-//     try{
-//     response = await fetch(`https://api.twitch.tv/helix/streams?game_id=${gId}`,{
-//         method:'GET',
-//         headers: {
-//         'Client-ID': 'iswx80n6way6l4cvuecpmtz3gw75vd'
-//         }
-//     });
-    
-//     feeds = await response.json();
-//     console.log('Sunygame', feeds);
-
-//     if(feeds.data.length > 0)
-//         return true;
-//     return false;
-//     }   
-//     catch(error){
-//         console.log(error);
-//         return false;
-//     }
-// }
-
-// //  For game [game-id]
-// //https://api.twitch.tv/helix/users?login=gorgc if user exists take id
-// //look with that id 
-// //https://api.twitch.tv/helix/streams?user_id=108268890
-// //Check if user name is present 'user:gorgc'
-// function matchesUserByUserName(userNameWithUser) {
-//     if(gIdWithBraces.trim().startsWith ('{') && gIdWithBraces.endsWith('}'))
-//     {
-//         let str = gIdWithBraces.trim();
-//         let ret = str.substr(1, str.length-2);
-//         console.log('|'+res+'|');
-//         if(ret.length >= 1)
-//             return {res: true, result: ret};
-//         return {res: false, result: null};
-//     }
-//     return {res: false,result: null};
-
-// }
-
-
-// //Check if a game exists with extracted correct formatted gameid
-// async function getStatusByUserName(userName){
-//     console.log(userName);
-//     let feedsOne, responseOne, feedsTwo, responseTwo;
-//     searchFound = false;
-//     try{
-//         responseOne = await fetch(`https://api.twitch.tv/helix/users?login=${userName}`,{
-//             method:'GET',
-//             headers: {
-//             'Client-ID': 'iswx80n6way6l4cvuecpmtz3gw75vd'
-//             }
-//         });
-        
-//         feedsOne = await responseOne.json();
-//         console.log('Sunyuser', feedsOne);
-//         let player_id = feedsOne.data[0].id;
-
-//         if(feedsOne.data.length > 0) {
-//             responseTwo = await fetch(`https://api.twitch.tv/helix/streams?user_id=${player_id}`,{
-//             method:'GET',
-//             headers: {
-//                 'Client-ID': 'iswx80n6way6l4cvuecpmtz3gw75vd'
-//                 }
-//             });
-//             feedsTwo = await responseTwo.json();
-//             console.log('Sunyuser', feedsTwo);
-//             if(feedsTwo.data[0].type=='live')
-//                 return {res: true, isLive:true};
-//             return {res:true, isLive:false};
-//         }
-//         else
-//             return {res:false, result: null};
-//     }   
-//     catch(error){
-//         console.log(error);
-//         return false;
-//     }
-// }
-
-// //Search for language
-
-// //https://api.twitch.tv/helix/streams?game_id=29595&first=100&language=pt
-// //For viewer count appli filter on result for viewer_count	>1000
-// //For live apply filter on result for type="live" or type=""
-// function matchesStreamByGameIdAndLang(langStr) {
-//     let str = langStr.trim();
-//     if(str.toLowerCase().startsWith('lang:')){
-//         let ret = str.slice(5);
-//         console.log('Language is' +ret);
-//         if(ret.length > 0)
-//             return {res: true, result: ret};
-//         return {res:false, result: null};
-//     }
-//     else{
-//         return {res:false, result: null};
-//     }
-// }
-
-// //Check if a game exists with extracted correct formatted gameid
-// async function getStreamByGameIdAndLang(gId, lang, countResult){
-//     console.log(gId);
-//     let feeds,response;
-//     searchFound = false;
-//     try{
-//     response = await fetch(`https://api.twitch.tv/helix/streams?game_id=${gId}&first=${countResult}&language=${lang}`,{
-//         method:'GET',
-//         headers: {
-//         'Client-ID': 'iswx80n6way6l4cvuecpmtz3gw75vd'
-//         }
-//     });
-    
-//     feeds = await response.json();
-//     console.log('Sunylang', feeds);
-
-//     if(feeds.data.length > 0)
-//         return true;
-//     return false;
-//     }   
-//     catch(error){
-//         console.log(error);
-//         return false;
-//     }
-// }
-
-
-// //https://api.twitch.tv/helix/streams?game_id=29595&first=100&language=pt
-// //For viewer count appli filter on result for viewer_count	>1000
-// function matchesStreamByGameIdAndViewerCount(viewerStr) {        //GameID could be optional here
-//     let str = viewerStr.trim();
-//     if(str.toLowerCase().startsWith('viewer:')||str.startsWith('viewers:')){
-//         let ind = str.indexOf(':');
-//         let ret = str.slice(ind+1);
-//         console.log('Viewer count' +ret);
-//         if(ret.length > 0)
-//             return {res: true, result: ret};
-//         return {res:false, result: null};
-//     }
-//     else{
-//         return {res:false, result: null};
-//     }
-// }
-
-// //Check if a game exists with extracted correct formatted gameid
-// async function getStreamByGameIdAndViewerCount(gId, lang, countResult, viewerCount){
-//     console.log(gId);
-//     let feeds,response;
-//     searchFound = false;
-//     try{
-//     response = await fetch(`https://api.twitch.tv/helix/streams?game_id=${gId}&first=${countResult}&language=${lang}`,{
-//         method:'GET',
-//         headers: {
-//         'Client-ID': 'iswx80n6way6l4cvuecpmtz3gw75vd'
-//         }
-//     });
-    
-//     feeds = await response.json();
-//     console.log('Sunylang', feeds);
-//     let cnt = 0;
-//     feeds.data.forEach(elem =>{
-//         if(elem.viewer_count > viewerCount) {
-//             cnt++;
-//         }
-//     })
-//     return cnt;
-//     }   
-//     catch(error){
-//         console.log(error);
-//         return false;
-//     }
-// }
-// search();
