8961213bb905d368ce509f0b92987b2f7a23305a
anshusaurav <anshu.saurav@gmail.com>
2020-04-28 21:23:28 +0530

    Featured-carousel

--- featured.js
+++ featured.js
@@ -7,6 +7,7 @@ let postIn;
 let preInPre;
 let postInPost;
 var embedOne, embedTwo, embedThree;
+var divArr = [];
 async function showTopic() {
     let feeds,response;
     response = await fetch(`https://api.twitch.tv/helix/streams`,{
@@ -26,11 +27,37 @@ async function showTopic() {
     postInPost = 2;
     preInPre = objArr.length-2;
     console.log()
-    loadInitialSliders(sliderIndex)
+    // loadInitialSliders(sliderIndex)
    
 }
-showTopic();
-function loadInitialSliders(sliderIndex){
-    let arrSlideers = [];
+let mainResultCont = document.querySelector('.featured-container');
+async function init(){
+    await showTopic();
+    let indArr = [preInPre, preIn,sliderIndex, postIn, postInPost];
+    for(let i =0; i < 5; i++){
+        let tempDiv = document.createElement('div');
+        let imgElem = document.createElement('img');
+        imgElem.src = getImageThumb(objArr[indArr[i]].thumbnail_url);
+        tempDiv.classList.add('featured-elem');
+        tempDiv.append(imgElem);
+        
+        mainResultCont.append(tempDiv);
+        divArr.push(tempDiv);
+    }
 
 }
+function moveLeft(){
+
+}
+function moveRight(){
+
+}
+
+function getImageThumb(str){
+    let ind = str.indexOf('-{width}x{height}');
+    let res = str.substr(0, ind) + str.substr(ind+17);
+    console.log(res);
+    return res;
+}
+// showTopic();
+init();
